322faa4474e30c25f774600df07038fe8dc904d6
Berkan Aydın <106911385+berkan2323@users.noreply.github.com>
2025-02-04 14:09:03 +0300

    Add files via upload

---
+++ CurrencyConverter/CurrencyConverterTests/CurrencyConverterTests.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterTests.swift
+//  CurrencyConverterTests
+//
+//  Created by Berkan Aydın on 1.01.2025.
+//
+
+import Testing
+@testable import CurrencyConverter
+
+struct CurrencyConverterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
